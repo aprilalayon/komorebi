9a43f65514369358228e8e5a4612a65e568917b4
April Alayon <eatingaprilapple@gmail.com>
2017-11-28 12:18:22 -0800

    Added functions lines
    Added CPT and taxonomies for Work, web and print

--- functions.php
+++ functions.php
@@ -130,6 +130,110 @@ function komorebi_scripts() {
 add_action( 'wp_enqueue_scripts', 'komorebi_scripts' );
 
 /**
+ * Custom Post Types
+ */
+function komorebi_register_custom_post_types() {
+
+    $labels = array(
+        'name'               => _x( 'Works', 'post type general name' ),
+        'singular_name'      => _x( 'Work', 'post type singular name'),
+        'menu_name'          => _x( 'Works', 'admin menu' ),
+        'name_admin_bar'     => _x( 'Work', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'Works' ),
+        'add_new_item'       => __( 'Add New Work' ),
+        'new_item'           => __( 'New Work' ),
+        'edit_item'          => __( 'Edit Work' ),
+        'view_item'          => __( 'View Work' ),
+        'all_items'          => __( 'All Works' ),
+        'search_items'       => __( 'Search Works' ),
+        'parent_item_colon'  => __( 'Parent Works:' ),
+        'not_found'          => __( 'No Work images found.' ),
+        'not_found_in_trash' => __( 'No Works found in Trash.' ),
+        'archives'           => __( 'Work Archives'),
+        'insert_into_item'   => __( 'Uploaded to this Work'),
+        'uploaded_to_this_item' => __( 'Work Archives'),
+        'filter_item_list'   => __( 'Filter Works list'),
+        'items_list_navigation' => __( 'Works list navigation'),
+        'items_list'         => __( 'Works list'),
+        'featured_image'     => __( 'Work feature image'),
+        'set_featured_image' => __( 'Set Work feature image'),
+        'remove_featured_image' => __( 'Remove Work feature image'),
+        'use_featured_image' => __( 'Use as feature image'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'Works' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'supports'           => array( 'title', 'thumbnail', 'editor' ),
+        'menu_icon'          => 'dashicons-format-quote',
+    );
+    register_post_type( 'work', $args );
+
+ }
+ add_action( 'init', 'komorebi_register_custom_post_types' );
+
+/** Rewrite flush **/
+function komorebi_rewrite_flush() {
+        komorebi_register_custom_post_types();
+        flush_rewrite_rules();
+    }
+
+add_action( 'after_switch_theme', 'komorebi_rewrite_flush' );
+
+
+/** Custom taxonomies **/
+function komorebi_register_taxonomies() {
+
+    $labels = array(
+        'name'                       => _x( 'Featured', 'taxonomy general name' ),
+        'singular_name'              => _x( 'Featured', 'taxonomy singular name' ),
+        'search_items'               => __( 'Search Featured' ),
+        'popular_items'              => __( 'Popular Featured' ),
+        'all_items'                  => __( 'All Featured' ),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __( 'Edit Featured' ),
+        'update_item'                => __( 'Update Featured' ),
+        'add_new_item'               => __( 'Add New Featured' ),
+        'new_item_name'              => __( 'New Featured Name' ),
+        'separate_items_with_commas' => __( 'Separate featured with commas' ),
+        'add_or_remove_items'        => __( 'Add or remove featured' ),
+        'choose_from_most_used'      => __( 'Choose from the most used featured' ),
+        'not_found'                  => __( 'No featured found.' ),
+        'menu_name'                  => __( 'Featured' ),
+    );
+
+    $args = array(
+
+        'hierarchical'          => true,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menu'      => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'featured' ),
+    );
+    register_taxonomy( 'featured', array( 'work' ), $args );
+}
+
+add_action( 'init', 'komorebi_register_taxonomies');
+
+
+
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
