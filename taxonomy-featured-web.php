8d8a125cd98e507270ab5c0f278f0a015f97467e
April Alayon <eatingaprilapple@gmail.com>
2018-01-09 07:42:42 -0800

    update

--- taxonomy-featured-web.php
+++ taxonomy-featured-web.php
@@ -10,8 +10,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="content-area web-content-area">
+		<main id="main" class="site-main web-main">
 
 		<?php
 		if ( have_posts() ) : ?>
